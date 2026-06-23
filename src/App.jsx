eac0cab84962ff13452352c3c966d5e1fc72185e
casjora <casjora@gmail.com>
2026-06-22 22:15:54 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import FakeAPI from './components/FakeAPI'
+
+function App() {
+
+  return (
+    <>
+      <FakeAPI/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FakeAPI.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export default function FakeAPI() {
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    async function db(params) {
+      try {
+        let response = await fetch("https://fakestoreapi.com/products");
+        let data = await response.json();
+        setApi(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    db();
+  }, []);
+
+  return (
+    <div>
+      {console.log(api)}
+      <p>Estas son las propiedades del API:</p>
+      <ul>
+        {/* Aseguramos que 'api' exista Y que tenga al menos un producto adentro */}
+        {api.length > 0 &&
+          Object.keys(api[0]).map((propiedad, i) => (
+            <li key={i}>{propiedad}</li>
+          ))}
+      </ul>
+      <p>Objeto Completo:</p>
+      {api.map((articulo, i) => (
+        <li key={i}>{articulo.title}</li>
+      ))}
+    </div>
+  );
+}
